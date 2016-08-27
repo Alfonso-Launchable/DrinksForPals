5247bfe0e7fed0c0b563d1ed62a0f9fbbd7cbf4b
Alfonso-Launchable <am.launchable@gmail.com>
2016-08-27 16:26:40 -0400

    js fix

--- js/Contact_Form.js
+++ js/Contact_Form.js
@@ -24,7 +24,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/jm.launchable@gmail.com",
+                url: "//formspree.io/admin@drinksforpals.com",
                 type: "POST",
 				dataType: "json",
                 data: {
@@ -40,7 +40,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>We just got your message, someone from the team will contact you shortly! :)  </strong>");
+                        .append("<strong>Thanks for subscribing, we hope to be in touch soon! :)  </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
